a15cb00a3679d186bdfad411c76e00ecceca929b
Drewpicus <drewpy.w.larsen@gmail.com>
2026-09-17 18:12:34 -0400

    quick inspect

--- Scripts/Components/player_controller_component.gd
+++ Scripts/Components/player_controller_component.gd
@@ -29,58 +29,71 @@ func _process(_delta: float) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact_menu"):
-		if not has_component(&"interactor"):
-			return
-		var mouse_position := root_entity.get_global_mouse_position()
-		var world := root_entity.get_world_2d().direct_space_state
-		var params := PhysicsPointQueryParameters2D.new()
-		params.position = mouse_position
-		params.collide_with_areas = true
-		params.collide_with_bodies = true
-		var intersections := world.intersect_point(params)
-		var visible_interactions: Array
-		var found_target := false
-		for intersection in intersections:
-			if intersection["collider"] is Entity:
-				if intersection["collider"] == root_entity:
-					continue
-				if not intersection["collider"].has_component(&"interactable"):
-					continue
-				visible_interactions = intersection["collider"].get_component(&"interactable").get_interactions(root_entity)
-				if not visible_interactions.is_empty():
-					interaction_menu.show_interactions(visible_interactions,root_entity,intersection["collider"])
-					found_target = true
-				break
-		if not found_target:
+		var target := _get_entity_under_mouse()
+		if not target:
 			interaction_menu.hide()
+			return
+		var visible_interactions: Array
+		if not target.has_component(&"interactable"):
+			return
+		visible_interactions = target.get_component(&"interactable").get_interactions(root_entity)
+		if not visible_interactions.is_empty():
+			interaction_menu.show_interactions(visible_interactions,root_entity,target)
 	if event.is_action_pressed("select"):
 		interaction_menu._on_empty_pressed()
 	if event.is_action_pressed("quick_attack"):
-		if not has_component(&"interactor"):
+		var target := _get_entity_under_mouse()
+		if not target:
 			return
-		if not has_component(&"stat_block"):
+		if not target.has_component(&"interactable"):
 			return
-		var mouse_position := root_entity.get_global_mouse_position()
-		var world := root_entity.get_world_2d().direct_space_state
-		var params := PhysicsPointQueryParameters2D.new()
-		params.position = mouse_position
-		params.collide_with_areas = true
-		params.collide_with_bodies = true
-		var intersections := world.intersect_point(params)
-		var visible_interactions: Array
-		for intersection in intersections:
-			if intersection["collider"] is Entity:
+		var interactions: Array[Interaction] = target.get_component(&"interactable").get_interactions(root_entity)
+		var selected_interaction: Interaction
+		if not interactions.is_empty():
+			for created_interaction in interactions:
+				if created_interaction.interaction_id == &"attack":
+					selected_interaction = created_interaction
+		if selected_interaction:
+			if selected_interaction.can_perform(root_entity,target):
+				selected_interaction.perform(root_entity,target)
+	_quick_action(event,"quick_inspect",&"info",&"interact",_get_entity_under_mouse(false))
+
+func _quick_action(event: InputEvent, input: StringName, component: StringName, interaction: StringName, target: Variant = _get_entity_under_mouse()):
+	if event.is_action_pressed(input):
+		if not target:
+			return
+		if not target.has_component(component):
+			return
+		var interactions: Array[Interaction] = target.get_component(&"interactable").get_interactions(root_entity)
+		var selected_interaction: Interaction
+		if not interactions.is_empty():
+			for created_interaction in interactions:
+				if created_interaction.interaction_id == interaction:
+					selected_interaction = created_interaction
+		if selected_interaction:
+			if selected_interaction.can_perform(root_entity,target):
+				selected_interaction.perform(root_entity,target)
+
+
+##Returns the first Entity at your mouse position. 
+##[param exclude_self] can be toggled off if you want to also include the root entity. 
+##[param is_interactable] can be toggled off to include non-interactable entities. 
+##[param include_areas] and [param include_bodies] set the PhysicsPointQueryParameters. 
+func _get_entity_under_mouse(exclude_self: bool = true, is_interactable: bool = true, include_areas: bool = true, include_bodies: bool = true) -> Entity:
+	var mouse_position := root_entity.get_global_mouse_position()
+	var world := root_entity.get_world_2d().direct_space_state
+	var params := PhysicsPointQueryParameters2D.new()
+	params.position = mouse_position
+	params.collide_with_areas = include_areas
+	params.collide_with_bodies = include_bodies
+	var intersections := world.intersect_point(params)
+	for intersection in intersections:
+		if intersection["collider"] is Entity:
+			if exclude_self:
 				if intersection["collider"] == root_entity:
 					continue
+			if is_interactable:
 				if not intersection["collider"].has_component(&"interactable"):
 					continue
-				visible_interactions = intersection["collider"].get_component(&"interactable").get_interactions(root_entity)
-				var attack_interaction: Interaction
-				if not visible_interactions.is_empty():
-					for created_interaction in visible_interactions:
-						if created_interaction.interaction_id == &"attack":
-							attack_interaction = created_interaction
-				if attack_interaction:
-					if attack_interaction.can_perform(root_entity,intersection["collider"]):
-						attack_interaction.perform(root_entity,intersection["collider"])
-				break
+			return intersection["collider"]
+	return null
---
+++ quick_actions.gd
@@ -0,0 +1,11 @@
+extends RefCounted
+class_name QuickAction
+
+const QUICK_ACTIONS: Dictionary = {
+	
+	
+	
+	
+}
+
+static func perform_quick_action():
