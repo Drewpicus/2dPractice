56a7f1214f2bf021a1d223957e9afe837f300f0a
Drewpicus <drewpy.w.larsen@gmail.com>
2026-09-20 00:14:55 -0400

    fixed item instances

---
+++ Scripts/Reference/item_component_registry.gd
@@ -0,0 +1,16 @@
+extends RefCounted
+class_name ItemComponentRegistry
+
+const COMPONENT_SCRIPTS: Dictionary = {
+	&"equippable": preload("res://equippable_item_component.gd"),
+}
+
+static func get_component_resource(component_name: StringName) -> ItemComponent:
+	var script = COMPONENT_SCRIPTS.get(component_name)
+	if not script:
+		return null
+	
+	return script.new() as ItemComponent
+
+static func get_component_name(component_name: StringName) -> String:
+	return component_name.capitalize().replace(" ","") + "ItemComponent"
--- item.gd
+++ item.gd
@@ -3,36 +3,40 @@ class_name Item
 
 var item_name: String
 var sprite: Texture2D
-var definition: ItemDefinition
+@export var definition: ItemDefinition
 var components: Array[ItemComponent]
 
 func get_component(id: StringName) -> ItemComponent:
-	if not definition:
-		return null
-	return definition.get_component(id)
+	for component in components:
+		if component and component.get_id() == id:
+			return component
+	return null
 
 func has_component(id: StringName) -> bool:
-	if not definition:
-		return false
-	return definition.has_component(id)
+	return get_component(id) != null
 
-func add_component(component:StringName,parameters:Dictionary={}) -> ItemComponent:
-	if not definition.components:
-		return
-
+func add_component(component: StringName, parameters: Dictionary = {}) -> ItemComponent:
 	if component.is_empty():
-		return
+		return null
 	
 	if has_component(component):
-		return
+		return null
 	
-	var new_component: ItemComponent = ItemComponent.new()
-	new_component.component_id = component
+	var new_component := ItemComponentRegistry.get_component_resource(component)
+	if not new_component:
+		return null
 	
+	for key in parameters:
+		if key in new_component:
+			new_component.set(key, parameters[key])
 	
-	if not parameters.is_empty():
-		for key in parameters.keys():
-			if key in new_component:
-				new_component.set(key,parameters[key])
-	
+	components.append(new_component)
 	return new_component
+
+func remove_component(id: StringName) -> ItemComponent:
+	var component := get_component(id)
+	if not component:
+		return null
+	
+	components.erase(component)
+	return component
