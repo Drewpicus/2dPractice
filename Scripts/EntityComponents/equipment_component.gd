7d1a0e6e0ebeafe2dcf9c334c58defc29712776c
Drewpicus <drewpy.w.larsen@gmail.com>
2026-09-21 23:56:53 -0400

    updated equipment component

--- Scripts/EntityComponents/equipment_component.gd
+++ Scripts/EntityComponents/equipment_component.gd
@@ -5,16 +5,34 @@ class_name EquipmentComponent
 @export var use_default_slots: bool = true
 var default_slots: Array[StringName] = [&"mainhand",&"offhand",&"head",&"chest",&"legs",&"feet"]
 var equipment: Dictionary[StringName,Item] = {}
+var _inventory: InventoryComponent
 
 signal equipment_updated
 
-func _ready() -> void:
+func on_added() -> void:
 	if use_default_slots:
-		slots.append_array(default_slots)
-		
-	var inventory := get_component(&"base:inventory") as InventoryComponent
-	if inventory:
-		inventory.item_removed.connect(_on_item_removed)
+		for slot in default_slots:
+			if slot not in slots:
+				slots.append(slot)
+
+	_set_inventory(get_component(&"base:inventory") as InventoryComponent)
+
+func on_removing() -> void:
+	_set_inventory(null)
+
+func _set_inventory(inventory: InventoryComponent) -> void:
+	if _inventory == inventory:
+		return
+
+	if _inventory:
+		if _inventory.item_removed.is_connected(_on_item_removed):
+			_inventory.item_removed.disconnect(_on_item_removed)
+
+	_inventory = inventory
+
+	if _inventory:
+		if not _inventory.item_removed.is_connected(_on_item_removed):
+			_inventory.item_removed.connect(_on_item_removed)
 
 func equip(slot: StringName, item: Item) -> bool:
 	if not item:
@@ -30,11 +48,10 @@ func equip(slot: StringName, item: Item) -> bool:
 	if slot not in equippable.slots:
 		return false
 	
-	var inventory = get_component(&"base:inventory") as InventoryComponent
-	if not inventory:
+	if not _inventory:
 		return false
-		
-	if item not in inventory.items:
+
+	if item not in _inventory.items:
 		return false
 	
 	for existing_slot in slots:
